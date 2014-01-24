4ec72d449861001d540be3462873c189f52fd327
accardoso <acc.intrisic@gmail.com>
2014-01-24 11:10:41 -0200

    Candidate map from different sources evaluation

---
+++ eval/src/main/scala/util/CandidateMapEval.scala
@@ -0,0 +1,18 @@
+package org.dbpedia.spotlight.util
+
+object CandidateMapEval {
+
+  /**
+   * Evaluations class for the candidate maps extracted (by ExtractCandidateMap class) from different sources,
+   * to define which sources combination is the best to use in Spotlight.
+   *
+   * @author Alexandre Cançado Cardoso - accardoso
+   */
+
+
+
+  def main(args: Array[String]){
+
+  }
+
+}
